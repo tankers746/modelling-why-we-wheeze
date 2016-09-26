19379d23fe8299aa5fb929a76d028104a4805f9c
AbstractSpaces <dj.bytheriver@gmail.com>
2016-09-26 14:31:15 +0800

    Debugging model demo pages
    Also changed airway_model to store radii and areas as objects instead of
    arrays, and simplified functions that respond to logd changes.

--- WebContent/demos/js/AHR_script_demo.js
+++ WebContent/demos/js/AHR_script_demo.js
@@ -1,6 +1,6 @@
 var AHR_demo = (function() {
     var channel = "demo/";
-    
+
     //These parameters do not change for the ASM page.
     var A = 2.75;
     var B = 1.5*Math.PI;
@@ -9,24 +9,24 @@ var AHR_demo = (function() {
     
     //Need to store these variables for when S changes and logd doesn't,
     //  and vice versa
-    var x=20;
+    var x=0.2;
     var y=0;
     var z=0;
     var logd=-6;
     
     //Create the model usign default starting values;
-    var model = new Airway(A, B, C, D, x, y, z);
+    var model = new Airway();
     
     //Callback function for when S changes
     function callback_S(e, S) {
         y = 0.01*S;
         z = 0.2*S;
         model.update(A, B, C, D, x, y, z, logd);
-        $.publish(channel + "ra", [model.radii[3]]);
-        $.publish(channel + "rb", [model.radii[2]]);
-        $.publish(channel + "rc", [model.radii[1]]);
-        $.publish(channel + "rd", [model.radii[0]]);
-        $.publish(channel + "AR", [model.tube_resistance(logd)]);
+        $.publish(channel + "ra", [model.radii.lumen]);
+        $.publish(channel + "rb", [model.radii.mucosal]);
+        $.publish(channel + "rc", [model.radii.sub_mucosal]);
+        $.publish(channel + "rd", [model.radii.asm]);
+        $.publish(channel + "AR", [model.resistance()]);
         $.publish(channel + "ASM_short", [model.shortening(logd)]);
         
     }
@@ -35,11 +35,11 @@ var AHR_demo = (function() {
     function callback_logd(e, logd_) {
         logd = logd_;
         model.update(A, B, C, D, x, y, z, logd);
-        $.publish(channel + "ra", [model.radii[3]]);
-        $.publish(channel + "rb", [model.radii[2]]);
-        $.publish(channel + "rc", [model.radii[1]]);
-        $.publish(channel + "rd", [model.radii[0]]);
-        $.publish(channel + "AR", [model.tube_resistance(logd)]);
+        $.publish(channel + "ra", [model.radii.lumen]);
+        $.publish(channel + "rb", [model.radii.mucosal]);
+        $.publish(channel + "rc", [model.radii.sub_mucosal]);
+        $.publish(channel + "rd", [model.radii.asm]);
+        $.publish(channel + "AR", [model.resistance()]);
         $.publish(channel + "ASM_short", [model.shortening(logd)]);
     }
     
--- WebContent/models/airway_model.js
+++ WebContent/models/airway_model.js
@@ -1,24 +1,18 @@
-// TODO: Set up the channel used to communicate airway data.
-// TODO: Setup event listeners to respond to user inputs.
-
-
+var defaults = {A:2.75, B:1.5, C:2.5, D:0.32, x:0.2,y:0,z:0};
 
 // Virtual representation of the airway being modelled.
-function Airway(A,B,C,D,x,y,z)
+function Airway()
 {
-    // The area of each airway layer, ordered from outermost to innermost.
-    this.areas = new Array(3);
-    
-    // The normal radius of the outermost layer with no methacoline present.
-    this.A = A;
-    
+	// The normal radius of the outermost layer with no methacoline present.
+    this.normal_asm;
     // The maximum percentage that the ASM is capable of shrinking.
-    this.max_short = x;
+    this.max_short;
+    // The area of each airway layer.
+    this.areas = new Object();
+    // The radii of the four circles that define the layers.
+    this.radii = new Object();
     
-    // The radii of the four circles that define the layers, also from outermost to innermost.
-    this.radii = new Array(4);
-    
-    this.update(A,B,C,D,x,y,z,-3);
+    this.update(defaults.A, defaults.B, defaults.C, defaults.D, defaults.x, defaults.y, defaults.z, -3);
 }
 
 // Calculates the radius of a circular layer given the radius and area of the layer above it.
@@ -28,9 +22,9 @@ Airway.prototype.layer_radius = function(above_radius, above_area)
 };
 
 // Poiseuille's Law.
-Airway.prototype.tube_resistance = function(radius)
+Airway.prototype.resistance = function()
 {
-    return 1 / Math.pow(radius, 4);
+    return 1 / Math.pow(this.radii.lumen, 4);
 };
 
 // ASM shortening triggered by a given methacoline dose.
@@ -39,38 +33,21 @@ Airway.prototype.shortening = function(logd)
     return this.max_short / (1 + Math.pow(10, -6-logd));
 };
 
-// The new airway layer dimensions after responding to a given methacoline dose.
-Airway.prototype.response = function(logd)
-{
-    radii = new Array(4);
-    
-    radii[0] = this.A * (1 - this.shortening(logd));
-    radii[1] = this.layer_radius(radii[0], this.areas[0]);
-    radii[2] = this.layer_radius(radii[1], this.areas[1]);
-    radii[3] = this.layer_radius(radii[2], this.areas[2]);
-    
-    return radii;
-};
-
-// Airway resistance triggered by a given methacoline dose. Resistance scales from 0 (normal airway) to 1 (fully closed airway).
-Airway.prototype.resistance = function(logd)
-{
-    lumen_radius = this.response(logd)[3];
-    normal_lumen = this.response(-3)[3];
-    
-    return 1 - (this.tube_resistance(normal_lumen) / this.tube_resistance(lumen_radius));
-};
-
 // Updates the airway properties in response to new inputs.
 Airway.prototype.update = function(A,B,C,D,x,y,z,logd)
 {
-    this.A = A;
+    this.normal_asm = A;
     this.max_short = x;
     
-    this.areas = [B, C*(1+y), D*(1+z)];
-    this.radii = this.response(logd);
+    this.areas.asm = B;
+    this.areas.sub_mucosal = C*(1+y);
+    this.areas.mucosal = D*(1+z);
     
+    this.radii.asm = this.normal_asm * (1 - this.shortening(logd));
+    this.radii.sub_mucosal = this.layer_radius(this.radii.asm, this.areas.asm);
+    this.radii.mucosal = this.layer_radius(this.radii.sub_mucosal, this.areas.sub_mucosal);
+    this.radii.lumen = this.layer_radius(this.radii.mucosal, this.areas.mucosal);
     
-    // TODO: Publish updated properties.
+	this.areas.lumen = Math.PI * Math.pow(this.radii.lumen, 2);
 };
 
