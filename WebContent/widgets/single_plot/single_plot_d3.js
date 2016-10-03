3d0383b656c58a01bcdc5214e472ca962d675a39
Michael Baxter <20503664@student.uwa.edu.au>
2016-10-03 16:35:38 +0800

    Refactored single_plot, removed marker animation.
    The code has been refactored to look much neater.  By removing the
    animation, the marker follows the line.  The widget loses some of its
    charm, though.

--- WebContent/widgets/single_plot/single_plot_d3.js
+++ WebContent/widgets/single_plot/single_plot_d3.js
@@ -1,120 +1,142 @@
 $.widget('mwww.single_plot_d3', {
     options: {
         channel:    "default/",
-        logd_topic: "logd",
-        model:      function(x) {return -(x+6)*(x+6);},
-        height:     300,
-        width:      400,
-        min:        -9,
-        max:        -3,
-        num_points: 50
+        topic_logd: "logd",
+        model:      function(x) {return x+9;},
+        
+        x_min:      -9,
+        x_max:      -3,
+        num_points: 50,
+        
+        aspect:     4/3,
+        
+        //animation_speed:    400,
     },
     
-    $container:   {},
-    callback_logd: {},
+    default_height:   300,
+    height:           0,
+    
+    svg:        {},
+    g:          {},
+    x_scale:    {},
+    y_scale:    {},
+    
+    path:       {},
+    x_axis:     {},
+    y_axis:     {},
+    marker:     {},
+    
+    
+    callback_logd:  function(e, logd_) {
+        //console.log("event");
+        /*this.marker.transition()
+            .attr("cx", this.x_scale(logd_))
+            .attr("cy", this.y_scale(this.options.model(logd_)))
+            .duration(this.options.animation_speed)
+            .ease(d3.easeLinear);*/
+        this.marker
+            .attr("cx", this.x_scale(logd_))
+            .attr("cy", this.y_scale(this.options.model(logd_)));
+    },
     
     
     _create: function() {
         
-        //var self = this;
-        //var options = this.options;
-        
-        this.$container = $("<div/>", {
-            id:     this.element.attr('id') + "_container",
-            height: this.options.height + "px",
-            width:  this.options.width + "px"
-            
-        }).css({'border' : '1px solid #000', 'position': 'relative', 'left': '50%', 'margin-left': -this.options.width/2 + "px"});
-        
-        $(this.element).append(this.$container);
+        //Resize widget if too small
+        if(this.element.height() < 1) {
+            this.element.height(this.default_height);
+            this.element.width(Math.floor(this.default_height * this.options.aspect));
+        }
         
         
+        //Find size for svg and set it to center.
+        this.height = Math.min(this.element.height(), Math.floor(this.element.width() / this.options.aspect));
+        this.element.css({"text-align": 'center'});
         
-        //Set a few constants
+        
+        //Create svg element
+        this.svg = d3.select(this.element.get(0)).append("svg")
+            .attr("height",  this.height)
+            .attr("width",   Math.floor(this.height * this.options.aspect))
+            .attr("style", "outline: 1px solid black;");
+        
+        
+        //Set up margins and x scale
         var margin = {top: 30, right: 30, bottom: 30, left: 30};
-        var inner_height = this.options.height - margin.top - margin.bottom;
-        var inner_width = this.options.width - margin.left - margin.right;
-        var x_scale = d3.scaleLinear().range([0, inner_width]);
-        var y_scale = d3.scaleLinear().range([inner_height, 0]);
+        var inner_height = this.svg.attr("height") - margin.top - margin.bottom;
+        var inner_width = this.svg.attr("width") - margin.left - margin.right;
+        this.x_scale = d3.scaleLinear()
+            .range([0, inner_width])
+            .domain([this.options.x_min, this.options.x_max]);
+            
         
-        x_scale.domain([this.options.min, this.options.max]);
         
-        
-        //Generate a set of points:
-        var step = (x_scale.domain()[1]-x_scale.domain()[0])/(this.options.num_points-1);
+        //Generate the points and set y scale.
+        var step = (this.x_scale.domain()[1]-this.x_scale.domain()[0])/(this.options.num_points-1);
         var data = [];
         
         for(var i=0; i<this.options.num_points; i++) {
-            data.push({
-                x: x_scale.domain()[0]+i*step,
-                y: this.options.model(x_scale.domain()[0]+i*step)
-            });
+            data.push({x: (this.x_scale.domain()[0]+i*step), y: 0});
+            data[i].y = this.options.model(data[i].x);
             //console.log(data[i].x + " " + data[i].y);
         }
         
-        y_scale.domain(d3.extent(data, function(d) {return d.y})).nice();
-        //console.log(y_scale.domain()[0] + " " + y_scale.domain()[1]);
+        this.y_scale = d3.scaleLinear()
+            .range([inner_height, 0])
+            .domain(d3.extent(data, function(d) {return d.y})).nice();
         
         
-        //Add an sgv canvas
-        var svg = d3.select(this.$container.get(0)).append("svg")
-            .attr("height",  this.options.height)
-            .attr("width", this.options.width);
-
-
-        //Plot line
-        var line = d3.line()
-            .x(function(d) {return x_scale(d.x);})
-            .y(function(d) {return y_scale(d.y);});
-
-        var g = svg.append("g")
+        //Create svg group element
+        this.g = this.svg.append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
         
-        var path = g.append("path")
+        
+        //Add axis
+        var x_axis = d3.axisBottom()
+            .scale(this.x_scale)
+            .ticks(this.x_scale.domain()[1] - this.x_scale.domain()[0])
+            .tickFormat(function (d) {return Math.pow(10,d+6);});
+            
+        this.g.append("g")
+            .attr("transform", "translate(0," + this.y_scale.range()[0] + ")").call(x_axis);
+        
+        var y_axis = d3.axisLeft()
+            .scale(this.y_scale);
+            
+        this.g.append("g")
+            .call(y_axis);
+        
+        //Plot line
+        var line = d3.line()
+            .x($.proxy(function(d) {return this.x_scale(d.x);}, this))
+            .y($.proxy(function(d) {return this.y_scale(d.y);}, this));
+            
+        this.path = this.g.append("path")
             .attr("d", line(data))
             .attr("fill", "none")
             .attr("stroke", "red")
             .attr("stroke-width", "3px");
-        
-        //Add axis
-        x_axis = d3.axisBottom().scale(x_scale)
-            .ticks(x_scale.domain()[1] - x_scale.domain()[0])
-            .tickFormat(function (d) {return Math.pow(10,d+6);});
-        
-        y_axis = d3.axisLeft().scale(y_scale);
-        
-        g.append("g").attr("transform", "translate(0," + y_scale.range()[0] + ")").call(x_axis);
-        g.append("g").call(y_axis);
-        
-        //Add position marker
-        var marker = g.append("circle")
+            
+        //Add marker
+        this.marker = this.g.append("circle")
             .attr("fill", "white")
             .attr("stroke", "black")
             .attr("stroke-width", 2)
             .attr("r", 6)
             .attr("cx", 0)
-            .attr("cy", y_scale.range()[0]);
-            
-            
-        //Animate position maker
-        var model = this.options.model;
-        this.callback_logd = function(e, logd_) {
-            //console.log("event");
-            marker.transition()
-                .attr("cx", x_scale(logd_))
-                .attr("cy", y_scale(model(logd_)));
-        };
+            .attr("cy", this.y_scale.range()[0]);
         
-        $.subscribe(this.options.channel + this.options.logd_topic, this.callback_logd);
-        
-        
+        //Set up subscriber
+        $.subscribe(this.options.channel + this.options.topic_logd, $.proxy(this.callback_logd, this));
         
     },
     
     _destroy: function() {
-        $.unsubscribe(this.options.channel + this.options.logd_topic, this.callback_logd);
+        //Unsubscribe
+        $.unsubscribe(this.options.channel + this.options.topic_logd, this.callback_logd);
+        
+        //Remove widget, but leave original div.
         $(this.element).empty();
-        $(this.element).remove();
     }
 
 });
