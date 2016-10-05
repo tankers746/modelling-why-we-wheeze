3f43ecb6e9865a1a5af137492e1a021d22fe67f4
AbstractSpaces <dj.bytheriver@gmail.com>
2016-10-05 14:03:06 +0800

    Fixed the error that caused lumen radius to go negative, and reconfigured the resistance function to accept a logd input.

--- WebContent/models/airway_model.js
+++ WebContent/models/airway_model.js
@@ -1,13 +1,10 @@
-// TODO: Add checks to prevent lumen area/radius becoming negative.
-// TODO: Alter resistance function to accept logd input (for drawing line graph).
-
-defaults = {A:2.75, B:1.5*Math.PI, C:2.5*Math.PI, D:0.32*Math.PI, x:0.2,y:0,z:0};
+defaults = {A:2.75, B:1.5*Math.PI, C:2.5*Math.PI, D:0.32*Math.PI, x:0.2,y:0,z:0,logd:-9};
 
 // Virtual representation of the airway being modelled.
 function Airway()
 {
 	// The normal radius of the outermost layer with no methacoline present.
-    this.normal_asm;
+    this.max_asm;
     // The maximum percentage that the ASM is capable of shrinking.
     this.max_short;
     // The area of each airway layer.
@@ -15,7 +12,7 @@ function Airway()
     // The radii of the four circles that define the layers.
     this.radii = new Object();
     
-    this.update(defaults.A, defaults.B, defaults.C, defaults.D, defaults.x, defaults.y, defaults.z, -3);
+    this.update(defaults.A, defaults.B, defaults.C, defaults.D, defaults.x, defaults.y, defaults.z, defaults.logd);
 };
 
 // Calculates the radius of a circular layer given the radius and area of the layer above it.
@@ -24,33 +21,59 @@ Airway.prototype.layer_radius = function(above_radius, above_area)
     return Math.sqrt(Math.pow(above_radius, 2) - above_area / Math.PI);
 };
 
-// Poiseuille's Law.
-Airway.prototype.resistance = function()
-{
-    return 1 / Math.pow(this.radii.lumen, 4);
-};
-
 // ASM shortening triggered by a given methacoline dose.
 Airway.prototype.shortening = function(logd)
 {
     return this.max_short / (1 + Math.pow(10, -6-logd));
 };
 
+// The shortened layer radii after receiving a methacholine dose.
+// Needed to separate this from the update() function so the resistance graph can be drawn without changing the Airway's properties.
+Airway.prototype.mc_response = function(areas, logd)
+{
+	var radii = new Object();
+
+	radii.asm = this.max_asm * (1 - this.shortening(logd));
+	radii.sub_mucosal = this.layer_radius(radii.asm, areas.asm);
+    radii.mucosal = this.layer_radius(radii.sub_mucosal, areas.sub_mucosal);
+    radii.lumen = this.layer_radius(radii.mucosal, areas.mucosal);
+
+    return radii;
+};
+
+// Poiseuille's Law.
+Airway.prototype.resistance = function(logd)
+{
+	return 1 / Math.pow(this.mc_response(this.areas, logd).lumen, 4);
+};
+
 // Updates the airway properties in response to new inputs.
+// Returns 0 upon successful update.
+// Returns -1 if airway cannot shrink further.
 Airway.prototype.update = function(A,B,C,D,x,y,z,logd)
 {
-    this.normal_asm = A;
+    var max_asm = A;
     this.max_short = x;
+
+    var areas = new Object();
     
-    this.areas.asm = B;
-    this.areas.sub_mucosal = C*(1+y);
-    this.areas.mucosal = D*(1+z);
-    
-    this.radii.asm = this.normal_asm * (1 - this.shortening(logd));
-    
-    this.radii.sub_mucosal = this.layer_radius(this.radii.asm, this.areas.asm);
-    this.radii.mucosal = this.layer_radius(this.radii.sub_mucosal, this.areas.sub_mucosal);
-    this.radii.lumen = this.layer_radius(this.radii.mucosal, this.areas.mucosal);
-    
-	this.areas.lumen = Math.PI * Math.pow(this.radii.lumen, 2);
+    areas.asm = B;
+    areas.sub_mucosal = C*(1+y);
+    areas.mucosal = D*(1+z);
+
+    var radii = this.mc_response(areas, logd);
+
+	// Check that the airway has enough area to contain all layers.
+    if(areas.asm + areas.sub_mucosal + areas.mucosal > Math.pow(radii.asm, 2)*Math.PI)
+    {
+    	return -1;
+	}
+	else
+	{
+		this.max_asm = max_asm;
+		this.areas = areas;
+		this.radii = radii;
+		
+		return 0;
+	}
 };
