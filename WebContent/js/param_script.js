6466832392d72cfc16f551faae8a53104a0a27aa
Tom <tankers746@live.com>
2016-10-12 23:00:45 +0800

    Spinner improvements

--- WebContent/js/param_script.js
+++ WebContent/js/param_script.js
@@ -99,13 +99,84 @@ var param = (function() {
 $(document).ready(function() {
     param.create();
     
-    $("#A_spinner").spinner2({channel: "mwww/", topic: "A", initval: 2.75, min: 1.75, max: 4.4, step: 0.05, pi: false});
-    $("#B_spinner").spinner2({channel: "mwww/", topic: "B", initval: 1.5,  min: 0.5,  max: 4,   step: 0.05, pi: true});
-    $("#C_spinner").spinner2({channel: "mwww/", topic: "C", initval: 2.5,  min: 1,    max: 6,   step: 0.1,  pi: true});
-    $("#D_spinner").spinner2({channel: "mwww/", topic: "D", initval: 0.32, min: 0.05, max: 2,   step: 0.01, pi: true});
-    $("#xmax_spinner").spinner2({channel: "mwww/", topic: "xmax", initval: 20,   min: 15,   max: 30,  step: 0.5,  pi: false});
-    $("#y_spinner").spinner2({channel: "mwww/", topic: "y", initval: 0,    min: 0,    max: 1,   step: 0.01, pi: false});
-    $("#z_spinner").spinner2({channel: "mwww/", topic: "z", initval: 0,    min: 0,    max: 5,   step: 0.05, pi: false});
+    $("#A_spinner").spinner2({
+        channel: "mwww/",
+        topic: "A",
+        initval: 2.75,
+        min: 1.75,
+        max: 4.4,
+        step: 0.05,
+        unit: "mm",
+        desc: "Airway outer radius"
+    });
+
+    $("#B_spinner").spinner2({
+        channel: "mwww/",
+        topic: "B",
+        initval: 1.5,
+        min: 0.5,
+        max: 4,
+        step: 0.05,
+        unit: "mm\u00B2",
+        suffix: '\u03C0',
+        desc: "ASM area"
+    });
+
+    $("#C_spinner").spinner2({
+        channel: "mwww/",
+        topic: "C",
+        initval: 2.5,
+        min: 1,
+        max: 6,
+        step: 0.1,
+        unit: "mm\u00B2",
+        suffix: '\u03C0',
+        desc: "Submucosal area"
+    });
+
+    $("#D_spinner").spinner2({
+        channel: "mwww/",
+        topic: "D",
+        initval: 0.32,
+        min: 0.05,
+        max: 2,
+        step: 0.01,
+        unit: "mm\u00B2",
+        suffix: '\u03C0',
+        desc: "Muscosal area"
+    });
+
+    $("#xmax_spinner").spinner2({
+        channel: "mwww/",
+        topic: "xmax",
+        initval: 20,
+        min: 15,
+        max: 30,
+        step: 0.5,
+        unit: "% max",
+        altLabel: "x<sub>max</sub>",
+        desc: "ASM shortening"
+    });
+
+    $("#y_spinner").spinner2({
+        channel: "mwww/",
+        topic: "y",
+        initval: 0,
+        min: 0,
+        max: 1,
+        step: 0.01,
+        desc: "Fractional increase in submucosa"
+    });
+
+    $("#z_spinner").spinner2({
+        channel: "mwww/",
+        topic: "z",
+        initval: 0,
+        min: 0,
+        max: 5,
+        step: 0.05,
+        desc: "Fractional increase in mucosa area"
+    });
     
     $("#cross_section").cross_section_d3({channel : "mwww/", rmax: 4.4});
     $("#dynamic_plot").dynamic_plot_d3({channel : "mwww/", model : param.resistance});
--- WebContent/widgets/spinner/spinner.js
+++ WebContent/widgets/spinner/spinner.js
@@ -1,82 +1,98 @@
+$.widget("mwww.spinner2", $.ui.spinner, {
 
-$.widget( "mwww.spinner2", $.ui.spinner, {
-    
     options: {
-        channel:    "mwww/",
-        topic:      "default_topic",
-        
+        channel: "mwww/",
+        topic: "default_topic",
+
         initval: 2.75,
-        min:    1.75,
-        max:    4.4,
-        step:   0.05,
-        pi:     false
+        min: 1.75,
+        max: 4.4,
+        step: 0.05,
+        unit: null,
+        desc: null,
+        altLabel: null,
+        suffix: null
     },
-    
-	 _format: function(value) { 
-		return value; 
-	 },
-	 
-	_parse: function(value) { 
-		//remove the pi when parsing values
-		return parseFloat(value.toString().split('\u03C0')[0]); 
-		},  
-	
+
+    _format: function(value) {
+        return value;
+    },
+
+    _parse: function(value) {
+        //remove the pi when parsing values
+        return parseFloat(value.toString().split('\0')[0]);
+    },
+
     _create: function() {
-        
+
         var self = this;
         var options = this.options;
-		
-        this.$label = $("<label/>", {
-            id: this.element.attr('id') + "_label",
-            text: this.options.topic + " "
+
+
+        var label = $("<label/>", {
+            html:  options.altLabel || options.topic
         });
-		
-        //$(this.element).wrap("<p></p>").before(this.$label);
-        //$(this.element).wrap("<span></span>").before(this.$label);
-        //$(this.element).wrap("<div></div>").before(this.$label);
-        $(this.element).before(this.$label);
-       
-        function checkInput(sp, initval, pi) {
+
+        $(this.element).wrap("<div class=param></div>");
+        $(this.element).before(label);
+        label.wrap("<div class=spinner_label></div>");
+
+        
+        if(options.unit) {
+            label.after(" (" + options.unit + ")");
+        }
+        if(options.desc) {
+            label.after(options.desc);
+        }
+
+
+        function updateValue(sp, options) {
             var value = $(sp).spinner2("value");
             var min = $(sp).spinner2("option", "min");
             var max = $(sp).spinner2("option", "max");
             //check if value is numerical
-            if (isNaN(parseFloat(value)) || !isFinite(value)){
-                value = initval;
+            if (isNaN(parseFloat(value)) || !isFinite(value)) {
+                value = options.initval;
             } else {
-                if(value > max) {
+                if (value > max) {
                     value = max;
-                } else if(value < min) {
+                } else if (value < min) {
                     value = min;
                 }
             }
-            //set value
-            if(pi) {
-                value = value + '\u03C0';
+            if (options.suffix) {
+                value += options.suffix;
             }
-            sp.value = value;		
-        }     	
+            /*
+            if(unit) {
+                value = value + " " + unit;
+            }*/
+            sp.value = value;
+            $.publish((options.channel + options.topic), [$(sp).spinner2("value")]);
+        }
         //create the spinner by calling super (this calls the create method of the class that we are extending - spinner)
-		this._super();
-		this._setOption("min", options.min);
-		this._setOption("max", options.max);
-		this._setOption("step", options.step);
-		this._setOption("change", function(event, ui) {
-							//ensures that the min and max are being adhered to
-							//also ensures that pi is added as a suffix
-							checkInput(this, options.initval, options.pi);
-							$.publish((self.options.channel+self.options.topic), [$(this).spinner2("value")]);
-						});
-		//set to default value
-		$(this.element).spinner2("value", options.initval); 
+        this._super();
+        this._setOption("min", options.min);
+        this._setOption("max", options.max);
+        this._setOption("step", options.step);
+
+        //ensures that the min and max are being adhered to
+        //also ensures that pi and unit are added
+        this._setOption("spin", function(event, ui) {
+            updateValue(this, options);
+        });
+        this._setOption("change", function(event, ui) {
+            updateValue(this, options);
+        });
+
+        //set to default value
+        $(this.element).spinner2("value", options.initval);
     },
-    
+
     _destroy: function() {
         this.$label.remove();
-		this._super();
-		$(this.element).unwrap();
-    }   
-    
+        this._super();
+        $(this.element).unwrap();
+    }
+
 });
-
-
